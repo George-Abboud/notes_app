1ee43e995cbcf4de3068b51341e77161230f8c8e
George-Abboud <geo1111ang@gmail.com>
2024-09-26 19:38:06 +0300

    create notes list view and add floating action button

---
+++ lib/views/widgets/custom_note_item_widget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CustomNoteItem extends StatelessWidget {
+  const CustomNoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Container(
+        decoration: BoxDecoration(
+            color: const Color(0xFFFDB851),
+            borderRadius: BorderRadius.circular(12)),
+        child: const Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                contentPadding: EdgeInsets.all(0),
+                title: Text(
+                  'Flutter Tips',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Text(
+                    'Build your flutter project with George Abboud',
+                    style: TextStyle(color: Colors.black38, fontSize: 14),
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.delete,
+                  size: 28,
+                  color: Colors.black,
+                ),
+              ),
+              Text(
+                'May 21,2022',
+                style: TextStyle(color: Colors.black26, fontSize: 12),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar_widget.dart';
+import 'package:notes_app/views/widgets/custom_note_item_widget.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -14,55 +15,23 @@ class NotesViewBody extends StatelessWidget {
             height: 48,
           ),
           CustomAppBar(),
-          CustomNoteItem()
+          NotesListView(),
         ],
       ),
     );
   }
 }
 
-class CustomNoteItem extends StatelessWidget {
-  const CustomNoteItem({super.key});
+class NotesListView extends StatelessWidget {
+  const NotesListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          color: const Color(0xFFFDB851),
-          borderRadius: BorderRadius.circular(12)),
-      child: const Padding(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              contentPadding: EdgeInsets.all(0),
-              title: Text(
-                'Flutter Tips',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w700),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                  'Build your flutter project with George Abboud',
-                  style: TextStyle(color: Colors.black38, fontSize: 14),
-                ),
-              ),
-              trailing: Icon(
-                Icons.delete,
-                size: 28,
-                color: Colors.black,
-              ),
-            ),
-            Text(
-              'May 21,2022',
-              style: TextStyle(color: Colors.black26, fontSize: 12),
-            ),
-          ],
-        ),
+    return Expanded(
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return const CustomNoteItem();
+        },
       ),
     );
   }
